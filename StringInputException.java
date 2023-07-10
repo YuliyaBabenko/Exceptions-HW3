00440a1a7633b5c46f03f72154eecd40b250e6fb
YuliyaB <114241910+YuliyaBabenko@users.noreply.github.com>
2023-07-10 13:22:52 +0300

    initial.commit

---
+++ StringInputException.java
@@ -0,0 +1,9 @@
+public class StringInputException extends RuntimeException{
+    public StringInputException() {
+        
+    }
+
+    public StringInputException(String message) {
+        super(message);
+    }
+}
